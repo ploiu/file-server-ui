796368758b1aaf4db0aab17746c4ae68ea9472b1
ploiu <ploiu415@gmail.com>
2023-10-15 19:26:13 -0400

    drag-n-drop basically a linked list of directories

---
+++ src/main/java/ploiu/model/FolderApproximation.java
@@ -0,0 +1,72 @@
+package ploiu.model;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Collection;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+/**
+ * represents a "middle ground" between a file system {@link File} and a {@link FolderApi}
+ *
+ * @param self the directory that this folder approximation is
+ */
+public record FolderApproximation(
+        File self,
+        Collection<File> childFiles,
+        Collection<FolderApproximation> childFolders
+) {
+    public FolderApproximation(@NotNull File self, @NotNull Collection<File> childFiles, @NotNull Collection<FolderApproximation> childFolders) {
+        Objects.requireNonNull(childFiles, "ChildFiles cannot be null.");
+        Objects.requireNonNull(childFolders, "ChildFolders cannot be null.");
+        Objects.requireNonNull(self, "Self cannot be null.");
+        if (!self.isDirectory()) {
+            throw new UnsupportedOperationException("Self must be a directory.");
+        }
+        this.self = self;
+        this.childFiles = childFiles;
+        this.childFolders = childFolders;
+        detectChildDirs();
+        detectSymLinks();
+    }
+
+    /**
+     * will throw an exception if any of our childFiles is a symlink, or if {@code self} is a symlink.
+     * This will include in the error message all of our direct child files that are a symlink, so the user can fix these
+     */
+    private void detectSymLinks() {
+        if (Files.isSymbolicLink(self.toPath())) {
+            throw new UnsupportedOperationException("Cannot upload symbolic links, fix these files paths: \n\t" + self.toPath());
+        }
+        var symPaths = childFiles
+                .stream()
+                .map(File::toPath)
+                .filter(Files::isSymbolicLink)
+                .map(Path::toAbsolutePath)
+                .map(Path::toString)
+                .collect(Collectors.joining("\n\t"));
+        if (!symPaths.isBlank()) {
+            throw new UnsupportedOperationException("Cannot upload symbolic links. Fix these file paths: \n\t" + symPaths);
+        }
+    }
+
+    /**
+     * will throw an exception if anything in childFiles is a directory.
+     * This represents an issue with the code, rather than an issue with the user
+     */
+    private void detectChildDirs() {
+        var childDirs = childFiles
+                .stream()
+                .filter(File::isDirectory)
+                .map(File::toPath)
+                .map(Path::toAbsolutePath)
+                .map(Path::toString)
+                .collect(Collectors.joining("\n\t"));
+        if (!childDirs.isBlank()) {
+            throw new UnsupportedOperationException("Your code is broken: directories are in the list of child files: \n\t" + childDirs);
+        }
+    }
+}
---
+++ src/test/java/ploiu/model/FolderApproximationTests.java
@@ -0,0 +1,63 @@
+package ploiu.model;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FolderApproximationTests {
+    static File root = new File("src/test/resources/FolderApproximationTests");
+
+    @BeforeEach
+    void setup() {
+        if (root.exists()) {
+            deleteDirectory(root);
+        }
+        root.mkdirs();
+    }
+
+    @AfterAll
+    static void teardown() {
+        deleteDirectory(root);
+    }
+
+    @Test
+    void testNullInputs() {
+        NullPointerException e;
+        e = assertThrows(NullPointerException.class, () -> new FolderApproximation(null, List.of(), List.of()));
+        assertEquals("Self cannot be null.", e.getMessage());
+        e = assertThrows(NullPointerException.class, () -> new FolderApproximation(new File(""), null, List.of()));
+        assertEquals("ChildFiles cannot be null.", e.getMessage());
+        e = assertThrows(NullPointerException.class, () -> new FolderApproximation(new File(""), List.of(), null));
+        assertEquals("ChildFolders cannot be null.", e.getMessage());
+    }
+
+    @Test
+    void testChildFilesNoDirs() {
+        var dir = new File(root.getAbsolutePath() + "/dir");
+        dir.mkdir();
+        var e = assertThrows(UnsupportedOperationException.class, () -> new FolderApproximation(root, List.of(dir), List.of()));
+        assertTrue(e.getMessage().startsWith("Your code is broken: directories are in the list of child files: \n\t"));
+    }
+
+    @Test
+    void testDetectSymLinks() {
+        fail("unimplemented");
+    }
+
+    // I didn't want to deal with writing this for a test. Thanks baeldung (https://www.baeldung.com/java-delete-directory)
+    private static boolean deleteDirectory(File directoryToBeDeleted) {
+        File[] allContents = directoryToBeDeleted.listFiles();
+        if (allContents != null) {
+            for (File file : allContents) {
+                deleteDirectory(file);
+            }
+        }
+        return directoryToBeDeleted.delete();
+    }
+
+}
